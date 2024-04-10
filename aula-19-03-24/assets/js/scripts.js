818b850f883cd8fff5c668408da12c9882684622
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-04-09 22:44:47 -0300

    code develped class day 09/04/24
    teacher André

--- aula-19-03-24/assets/js/scripts.js
+++ aula-19-03-24/assets/js/scripts.js
@@ -21,26 +21,37 @@ function appendToDisplay(value) {
 }
 
 function operator(value) {
+  // alert('Fase 1:\n' + numbers_stack + '\n' + operator_stack);
   if (!number_complete) {
     addNumberToStack();
-    addOperatorToStack(value);
+    document.getElementById('expression').value +=
+      ' ' + topOfOperatorNumber() + ' ' + value;
+    // alert('Fase 2:\n' + numbers_stack + '\n' + operator_stack);
+    while (
+      lenghtOfOperatorStack() > 0 &&
+      !precedence(topOfOperatorStack(), value)
+    ) {
+      let result = partialCalculate();
+      addResultToStack(result);
+      // alert('Fase 3:\n' + numbers_stack + '\n' + operator_stack);
+    }
   } else {
-    remOperatorFromStack(value);
-    addOperatorToStack(value);
+    getOperatorFromStack(value);
   }
+  addOperatorToStack(value);
+  // alert('Fase 4:\n' + numbers_stack + '\n' + operator_stack);
 }
 
 function calculate() {
   addNumberToStack();
-  // alert(numbers_stack + '\n' + operator_stack);
-
-  let result = partialCalculate();
+  document.getElementById('expression').value +=
+    ' ' + topOfOperatorNumber() + ' =';
+  let result = 0;
+  while (lenghtOfOperatorStack() > 0) {
+    result = partialCalculate();
+    addResultToStack(result);
+  }
   document.getElementById('display').value = result;
-  number_complete = false;
-
-  // reset temporário
-  numbers_stack = [];
-  operator_stack = [];
 }
 
 function clearDisplay() {
@@ -62,18 +73,45 @@ function addNumberToStack() {
   numbers_stack.push(number);
 }
 
+function addResultToStack(number) {
+  numbers_stack.push(number);
+}
+
 function addOperatorToStack(value) {
   operator_stack.push(value);
 }
 
-function remOperatorFromStack(value) {
-  operator_stack.pop();
+function getOperatorFromStack(value) {
+  return operator_stack.pop();
 }
 
 function partialCalculate() {
   let n2 = numbers_stack.pop();
   let n1 = numbers_stack.pop();
-  let op = operator_stack.pop();
+  let op = getOperatorFromStack();
   let result = eval(n1 + op + n2);
   return result;
+}
+
+function precedence(op1, op2) {
+  let operators = new Map([
+    ['+', 1],
+    ['-', 1],
+    ['*', 2],
+    ['/', 2],
+    ['**', 3],
+  ]);
+  return operators.get(op2) > operators.get(op1);
+}
+
+function topOfOperatorStack() {
+  return operator_stack[operator_stack.length - 1];
+}
+
+function topOfOperatorNumber() {
+  return numbers_stack[numbers_stack.length - 1];
+}
+
+function lenghtOfOperatorStack() {
+  return operator_stack.length;
 }
